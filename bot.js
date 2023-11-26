bac1705f93ea5471508c33783609fbdaaaaaae36
Heneros <punk60826@gmail.com>
2023-11-26 14:35:30 +0200

    ...

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
+const webAppUrl = 'https://deft-puppy-88f707.netlify.app';
 
 bot.setMyCommands([
     { command: '/start', description: 'Welcome, guest!' }
@@ -16,14 +17,14 @@ bot.on('message', async (msg) => {
         await bot.sendMessage(chatId, 'Below you see button, fill the form', {
             reply_markup: {
                 keyboard: [
-                    [{ text: 'Fill form' }]
+                    [{ text: 'Fill form', web_app: { url: webAppUrl } }]
                 ]
             }
         })
-        await bot.sendMessage(chatId, 'Below you see button, fill the form', {
+        await bot.sendMessage(chatId, 'List our magazine', {
             reply_markup: {
-                inline_keyboardkeyboard: [
-                    [{ text: 'Make order' }]
+                inline_keyboard: [
+                    [{ text: 'Make order', web_app: { url: webAppUrl } }]
                 ]
             }
         })
